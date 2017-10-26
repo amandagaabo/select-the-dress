d8a65ee441a129c653ba52a244ab65e48f245e9f
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-10-25 18:59:59 -0600

    updated start server function

--- server.js
+++ server.js
@@ -82,7 +82,8 @@ app.use('*', function (req, res) {
 let server
 
 // this function connects to our database, then starts the server
-function runServer (databaseUrl = DATABASE_URL, port = PORT) {
+// function runServer (databaseUrl = DATABASE_URL, port = PORT) {
+function runServer (databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, {useMongoClient: true}, err => {
       if (err) {
